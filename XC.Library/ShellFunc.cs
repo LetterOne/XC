dbdaf46040fd0261de1dff3aa02529b7ca7871de
军强 沈 <1137023772@qq.com>
2019-06-12 20:37:18 +0800

    添加执行Shell脚本公共类

---
+++ XC.Library/ShellFunc.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XC.Library
+{
+    class ShellFunc
+    {
+        public static int SSHLogin(string strHost, string strUser, string strPwd, ref string strPush)
+        {
+
+            try
+            {
+                ShellHelper shell = new ShellHelper();
+                if (shell.OpenShell(strHost, strUser, strPwd))
+                {
+                    strPush = "SSH Connect OK!";
+                    shell.Close();//关闭连接 
+                }
+            }
+            catch (Exception ex)
+            {
+                strPush = ex.ToString();
+                return -1;
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// 传入Shell命令脚本
+        /// </summary>
+        /// <param name="List">Shell命令集</param>
+        /// <param name="strHost">主机地址</param>
+        /// <param name="strUser">用户名</param>
+        /// <param name="strPwd">密码</param>
+        /// <param name="strPush">返回信息</param>
+        /// <returns></returns>
+       public static int SSHCommands(List<string> List, string strHost, string strUser, string strPwd, ref string strPush)
+        {
+            try
+            {
+                strPush = "";
+                ShellHelper shell = new ShellHelper();
+                //连接linux成功 
+                if (shell.OpenShell(strHost, strUser, strPwd))
+                {
+                    foreach (string list in List)
+                    {
+                        shell.Shell(list);//执行获取命令  
+                        System.Threading.Thread.Sleep(200);
+                    }
+                    strPush = shell.GetAllString();//获取返回结果 
+                    shell.Close();//关闭连接 
+                }
+            }
+            catch (Exception ex)
+            {
+                strPush = ex.ToString();
+                return -1;
+            }
+            return 0;
+        }
+    }
+}
---
+++ XC.Library/ShellHelper.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Tamir.SharpSsh.jsch;
+
+namespace XC.Library
+{
+    /// <summary>
+    /// Shell脚本输入
+    /// </summary>
+    class ShellHelper
+    {
+
+        System.IO.MemoryStream outputstream = new MemoryStream();
+        Tamir.SharpSsh.SshStream inputstream = null;
+        Channel channel = null;
+        Session session = null;
+        /// <summary> 
+        /// 命令等待标识 
+        /// </summary> 
+        string waitMark = "]#";
+
+        /// <summary> 
+        /// 打开连接 
+        /// </summary> 
+        /// <paramname="host"></param> 
+        /// <paramname="username"></param> 
+        /// <paramname="pwd"></param> 
+        /// <returns></returns> 
+        public bool OpenShell(string host, string username, string pwd)
+        {
+            try
+            {
+                ////Redirect standard I/O to the SSHchannel 
+                inputstream = new Tamir.SharpSsh.SshStream(host, username, pwd);
+                ///我手动加进去的方法。。为了读取输出信息 
+                inputstream.set_OutputStream(outputstream);
+                return inputstream != null;
+            }
+            catch { throw; }
+
+        }
+
+        /// <summary> 
+        /// 执行命令 
+        /// </summary> 
+        /// <paramname="cmd"></param> 
+        public bool Shell(string cmd)
+        {
+            if (inputstream == null) return false;
+            string initinfo = GetAllString();
+            inputstream.Write(cmd);
+            inputstream.Flush();
+            string currentinfo = GetAllString();
+            while (currentinfo == initinfo)
+            {
+                System.Threading.Thread.Sleep(100);
+                currentinfo = GetAllString();
+            }
+            return true;
+        }
+
+        /// <summary> 
+        /// 获取输出信息 
+        /// </summary> 
+        /// <returns></returns> 
+        public string GetAllString()
+        {
+            string outinfo = Encoding.UTF8.GetString(outputstream.ToArray());
+            //等待命令结束字符 
+            while (!outinfo.Trim().EndsWith(waitMark))
+            {
+                System.Threading.Thread.Sleep(200);
+                outinfo = Encoding.UTF8.GetString(outputstream.ToArray());
+            }
+            outputstream.Flush();
+            return outinfo.ToString();
+        }
+
+        /// <summary> 
+        /// 关闭连接 
+        /// </summary> 
+        public void Close()
+        {
+            if (inputstream != null) inputstream.Close();
+        }
+    }
+}
