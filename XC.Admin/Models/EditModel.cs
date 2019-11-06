9bc783d87a932d9b25f22c6eec67009d530e1fc5
军强 沈 <1137023772@qq.com>
2019-11-06 21:13:18 +0800

    新增WebApp

---
+++ XC.Admin/Models/EditModel.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XC.Admin.Models
+{
+    public class EditModel
+    {
+        public EditModel()
+        {
+
+        }
+        [BindProperty]
+        public Instructor Instructor { get; set; }
+    }
+
+    [BindProperties(SupportsGet = true)]
+    public class CreateModel : InstructorsPageModel
+    {
+        public CreateModel() : base()
+        {
+        }
+
+        public Instructor Instructor { get; set; }
+        [BindNever]
+        public int ID { get; set; }
+        [BindProperty(Name = "ai_user", SupportsGet = true)]
+        public string ApplicationInsightsCookie { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Hire Date")]
+        [BindRequired]
+        public DateTime HireDate { get; set; }
+
+        [FromQuery(Name = "Note")]
+        public string NoteFromQueryString { get; set; }
+
+        public void OnGet([FromHeader(Name = "Accept-Language")] string language)
+        {
+
+        }
+    } 
+}
