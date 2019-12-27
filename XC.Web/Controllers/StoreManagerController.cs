0d8ba1d24284cda7f442fc928efd7e1c9232c734
军强 沈 <1137023772@qq.com>
2019-12-27 14:15:30 +0800

    添加MVC EF支持

---
+++ XC.Web/Controllers/StoreManagerController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using XC.Web.Models;
+
+namespace XC.Web.Controllers
+{
+    public class StoreManagerController : Controller
+    {
+        private readonly MusicStoreDB _context;
+
+        public StoreManagerController(MusicStoreDB context)
+        {
+            _context = context;
+        }
+
+        // GET: StoreManager
+        public async Task<IActionResult> Index()
+        {
+            var musicStoreDBContext = _context.Album.Include(a => a.Artist).Include(a => a.Genre);
+            return View(await musicStoreDBContext.ToListAsync());
+        }
+
+        // GET: StoreManager/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var album = await _context.Album
+                .Include(a => a.Artist)
+                .Include(a => a.Genre)
+                .FirstOrDefaultAsync(m => m.AlbumID == id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+
+            return View(album);
+        }
+
+        // GET: StoreManager/Create
+        public IActionResult Create()
+        {
+            ViewData["ArtistID"] = new SelectList(_context.Set<Artist>(), "ArtistID", "ArtistID");
+            ViewData["GenreID"] = new SelectList(_context.Set<Genre>(), "GenreID", "GenreID");
+            return View();
+        }
+
+        // POST: StoreManager/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AlbumID,GenreID,ArtistID,Title,Price,AlbumArtUri")] Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(album);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ArtistID"] = new SelectList(_context.Set<Artist>(), "ArtistID", "ArtistID", album.ArtistID);
+            ViewData["GenreID"] = new SelectList(_context.Set<Genre>(), "GenreID", "GenreID", album.GenreID);
+            return View(album);
+        }
+
+        // GET: StoreManager/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var album = await _context.Album.FindAsync(id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+            ViewData["ArtistID"] = new SelectList(_context.Set<Artist>(), "ArtistID", "ArtistID", album.ArtistID);
+            ViewData["GenreID"] = new SelectList(_context.Set<Genre>(), "GenreID", "GenreID", album.GenreID);
+            return View(album);
+        }
+
+        // POST: StoreManager/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AlbumID,GenreID,ArtistID,Title,Price,AlbumArtUri")] Album album)
+        {
+            if (id != album.AlbumID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(album);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AlbumExists(album.AlbumID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ArtistID"] = new SelectList(_context.Set<Artist>(), "ArtistID", "ArtistID", album.ArtistID);
+            ViewData["GenreID"] = new SelectList(_context.Set<Genre>(), "GenreID", "GenreID", album.GenreID);
+            return View(album);
+        }
+
+        // GET: StoreManager/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var album = await _context.Album
+                .Include(a => a.Artist)
+                .Include(a => a.Genre)
+                .FirstOrDefaultAsync(m => m.AlbumID == id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+
+            return View(album);
+        }
+
+        // POST: StoreManager/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var album = await _context.Album.FindAsync(id);
+            _context.Album.Remove(album);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AlbumExists(int id)
+        {
+            return _context.Album.Any(e => e.AlbumID == id);
+        }
+    }
+}
---
+++ XC.Web/Data/MusicStoreDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace XC.Web.Models
+{
+    public class MusicStoreDB : DbContext
+    {
+        public MusicStoreDB (DbContextOptions<MusicStoreDB> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<XC.Web.Models.Album> Album { get; set; }
+    }
+}
